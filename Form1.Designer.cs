cb4d87b9c90d1aa5f60d14deab4df647ed78cbab
knasiak <pio.knasiak@gmail.com>
2022-11-29 18:26:37 +0100

    Początek

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.pnlLogin.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.btnTest);
+            this.pnlLogin.Controls.Add(this.lblTest);
+            this.pnlLogin.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(1159, 870);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(469, 460);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(94, 29);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "button1";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(373, 295);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(50, 20);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1159, 871);
+            this.Controls.Add(this.pnlLogin);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Name me";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel pnlLogin;
+        private Label lblTest;
+        private Button btnTest;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace Projest_Remake_2022
 {
     public partial class Form1 : Form
@@ -10,7 +13,35 @@ namespace Projest_Remake_2022
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            locations.Add(pnlLogin);
+            //locations.Add();
+            //locations.Add();
+            //locations.Add();
+            //locations.Add();
+            void ChangeWidth(object sender, EventArgs e)
+            {
+                pnlLogin.Width = this.Width-22;
+            }
+
+            this.SizeChanged+=ChangeWidth;
         }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            void FindMiddle(object obj)
+            {
+                if (obj.GetType() == typeof(Panel))
+                {
+                    Panel pnl = (Panel)obj;
+                    lblTest.Text = (pnl.Width / 2).ToString();
+                }
+            }
+
+            Stopwatch time = new Stopwatch();
+
+            time.Start();
+            FindMiddle(pnlLogin);
+        }
+        
     }
 }
